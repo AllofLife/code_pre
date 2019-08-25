9ce8f1189cdf6914c5cb9b2768147da6b4c505b9
AllofLife <742415987@qq.com>
2019-08-25 16:04:52 +0800

    hard

--- common_solution.cpp
+++ common_solution.cpp
@@ -20,41 +20,61 @@ private:
 public:
      Solution(/* args */ );
     ~ Solution();
-    void nextPermutation(vector<int>& nums) {
-        if (nums.size()==0)
+    int search(vector<int>& nums, int target) {
+         if (nums.size() == 0)
         {
-           return ;
+            return -1;
         }
-        
-        // int max_order = nums[0];
-        int i  =nums.size() -2 ;
-
-        for (; i >= 0; i--)
+        if (nums.size() == 1)
         {
-            // 找到第一个下降的地方
-            if (nums[i] < nums[i + 1])
+            if (nums[0]==target)
             {
-                // cout<<nums[i]<<nums[i+1]<<endl;
-                break;
+               return 0;
+            }
+            else
+            {
+                return -1;
+            }
+              
+        }
+        if (nums.size() == 2)
+        {
+            if (nums[0]==target)
+            {
+               return 0;
+            }
+            else if(nums[1]==target)
+            {
+                return 1;
             }
             
-        }
-        if(i >= 0){
-            int j = nums.size() - 1;
-            while (j >= 0 && nums[j] <= nums[i])
+            else
             {
-               j--;
+                return -1;
             }
-            // cout<<nums[i]<<nums[j]<<endl;
-            int tmp = nums[j];
-            nums[j] = nums[i];
-            nums[i] = tmp;
-            // return;
         }
-        // cout<<"i"<<i<<endl;
-        // 把后面的数排序
-        sort(nums.begin()+i+1,nums.end());
         
+        int vsize = nums.size();
+        int i = vsize - 1;
+        while (i >= 0 && nums[i] > nums[i -1])
+        {
+            i--;
+        }
+        cout<<"i "<<i<<nums[i]<<endl;
+        cout<<"size - 1 "<<nums[vsize -1]<<endl;
+        
+        if (nums[i] <= target && nums[vsize - 1] >= target )
+        {
+           return binary_search(nums.begin() + i,nums.end(),target) + 1;
+        }
+        else if (nums[0] <= target && nums[i - 1] >= target)
+        {
+            cout<<"i"<<i<<"target"<<target<<endl;
+           return binary_search(nums.begin(),nums.begin()+ i -1,target) - 1;
+        }
+        return -1;
+    
+          
     }
 };
  Solution:: Solution(/* args */)
@@ -85,21 +105,19 @@ int main(int argc,char** argv){
     
     cout<<"31 "<<endl;
     vector<int> nums;
-    // nums.push_back(1);
-    // nums.push_back(2);
-    // nums.push_back(3);
-    // nums.push_back(1);
-    // nums.push_back(2);
-    // nums.push_back(3);
     nums.push_back(1);
     nums.push_back(3);
-    nums.push_back(2);
-
+    // nums.push_back(4);
+    // nums.push_back(5);
+    // nums.push_back(6);
+    // nums.push_back(7);
+    // nums.push_back(0);
+    // nums.push_back(1);
+    // nums.push_back(2);
 
     Solution s;
     int size =nums.size();
-    // int size = s.nextPermutation(nums);
-    s.nextPermutation(nums);
+    cout<<s.search(nums,0)<<endl;
     for_each(nums.begin(),nums.begin() + size,[](int ele){cout<<ele<<" ";});
     cout<<endl;
     
