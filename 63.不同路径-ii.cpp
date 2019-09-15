673e00f0ef0ed4b1652cc0a2f34e49eb9b463233
AllofLife <742415987@qq.com>
2019-09-15 10:15:09 +0800

    背包问题 动态规划 unsigned int

--- 63.不同路径-ii.cpp
+++ 63.不同路径-ii.cpp
@@ -1,27 +1,35 @@
 /*
- * @lc app=leetcode.cn id=62 lang=cpp
+ * @lc app=leetcode.cn id=63 lang=cpp
  *
- * [62] 不同路径
+ * [63] 不同路径 II
  */
 class Solution {
 public:
-      int uniquePaths(int m, int n) {
+        int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
         // 试试排列组合 在c++里头怎么使用的
         //   int ans = boost::math::factorial(flaot(m + n - 2)) / boost::math::factorial(float(m - 1)) / boost::math::factorial(float(n - 1 ));
         // return ans;
         // 也许是背包问题
-        // vector<vector<int>> dp;
+        // vector<vector<int>> obstacleGrid;
         //  先计算从(0,0)出发 到达 (i,j)的最小路径
         // 记录在数组里 遍历一遍就能把任意一个位置的路径找出来了.
         // 到达第一横排(0,i)坐标的路径就只有一条,同理的还有第一竖排坐标(i,0).
-        int dp[m][n]={0};
-        for (int i = 0; i < m; i++)
+        unsigned int dp[m][n]={0};
+        if (obstacleGrid[0][0] == 1)
         {
-            dp[i][0] = 1;
+            return 0;
         }
-        for (int i = 0; i < n; i++)
+        dp[0][0] = 1;
+        
+        for (int i = 1; i < m; i++)
         {
-            dp[0][i] = 1;
+            dp[i][0] = (obstacleGrid[i][0]==0 && dp[i -1][0] == 1)? 1:0;
+        }
+        for (int i = 1; i < n; i++)
+        {
+            dp[0][i] = (obstacleGrid[0][i]==0 && dp[0][i - 1] == 1)? 1:0;
         }
         // 到达任意一个坐标(i,j)的路径 计算方法为 
         // 到达 (i- 1)(j)的路径数加上(i)(j-1)的路径路 
@@ -30,7 +38,16 @@ public:
         {
             for (int j = 1; j < n; j++)
             {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                if (obstacleGrid[i][j] == 0)
+                {
+                  dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+                else
+                {
+                    dp[i][j] = 0;
+                }
+                
+                
             }
             
         }
@@ -38,5 +55,6 @@ public:
         return dp[m - 1][n - 1];
 
     }
+
 };
 
--- common_solution.cpp
+++ common_solution.cpp
@@ -19,155 +19,55 @@ class  Solution
 public:
      Solution(/* args */ );
     ~ Solution();
-       vector<int> spiralOrder(vector<vector<int>>& matrix) {
-         vector<int>  ans;
-        if (matrix.empty())
+      int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        // 试试排列组合 在c++里头怎么使用的
+        //   int ans = boost::math::factorial(flaot(m + n - 2)) / boost::math::factorial(float(m - 1)) / boost::math::factorial(float(n - 1 ));
+        // return ans;
+        // 也许是背包问题
+        // vector<vector<int>> obstacleGrid;
+        //  先计算从(0,0)出发 到达 (i,j)的最小路径
+        // 记录在数组里 遍历一遍就能把任意一个位置的路径找出来了.
+        // 到达第一横排(0,i)坐标的路径就只有一条,同理的还有第一竖排坐标(i,0).
+        // int obstacleGrid[m][n]={0};
+        if (obstacleGrid[0][0] == 1)
         {
-            return ans;
+            return 0;
         }
-         
-        // vector<vector<int>> vv_bool;
-        // 调试真的好难呀
-        int R_size = matrix.size(), C_size = matrix[0].size();
-        // cout<<R_size<<C_size<<endl;
-        int vv_bool[R_size][C_size] = {0,0} ;
-        for (int i = 0; i < R_size; i++)
-        {
-           for (int j = 0; j < C_size; j++)
-           {
-            //   cout<<vv_bool[i][j]<<" ";
-            vv_bool[i][j] = 0;
-           }
-
-           
-        }
-       
+        obstacleGrid[0][0] = 1;
         
-        int r = 0,c = 0;
-        int dic_r[4] = {0,1,0,-1};
-        int dic_c[4] = {1,0,-1,0};
-        int det = 0;
-        // 前进的方向
-        for (int i = 0; i < R_size * C_size; i++)
+        for (int i = 1; i < m; i++)
         {
-            // cout<<matrix[r][c]<<" i "<<i<<endl;
-            ans.push_back(matrix[r][c]);
-            vv_bool[r][c] = i + 1;
-            int cr = r + dic_r[det];
-            int cc = c + dic_c[det];
-            // cout<<"det "<<det<<" cr "<<cr<<" cc "<<cc<<endl;
-            
-            
-            // 确保 matrix[r][c] 没有超出范围
-            if (0 <= cc && cc < C_size && 0 <= cr && cr < R_size  && vv_bool[cr][cc] == 0)
+            obstacleGrid[i][0] = (obstacleGrid[i][0]==0 && obstacleGrid[i -1][0] == 1)? 1:0;
+        }
+        for (int i = 1; i < n; i++)
+        {
+            obstacleGrid[0][i] = (obstacleGrid[0][i]==0 && obstacleGrid[0][i - 1] == 1)? 1:0;
+        }
+        // 到达任意一个坐标(i,j)的路径 计算方法为 
+        // 到达 (i- 1)(j)的路径数加上(i)(j-1)的路径路 
+        // 因为这两个点到目标位置的路径只有一条了
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
             {
-            
-                // 还在范围内就继续直走
-                r = cr;
-                c = cc;
-                // cout<<" r " <<r<<" c "<<c<<endl;
-
-            }
-            else
-            {
-                // cout<<"转弯"<<endl;
-                // 不在范围内就转个弯
-                det = (det + 1) % 4;
-                // cout<<" else det "<<det<<endl;
-                r = r + dic_r[det];
-                c = c + dic_c[det];
-                // cout<<" r " <<r<<" c "<<c<<endl;
+                if (obstacleGrid[i][j] == 0)
+                {
+                  obstacleGrid[i][j] = obstacleGrid[i - 1][j] + obstacleGrid[i][j - 1];
+                }
+                else
+                {
+                    obstacleGrid[i][j] = 0;
+                }
                 
-
-            }
-            
-            
-        }
-         for (int i = 0; i < R_size; i++)
-        {
-           for (int j = 0; j < C_size; j++)
-           {
-              cout<<vv_bool[i][j]<<" ";
-            // vv_bool[i][j] = 0;
-           }
-
-           cout<<endl;
-        }
-        
-        return ans;
-    }
-    vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> vvtest;
-        vvtest.resize(n);
-        
-        for (int i = 0; i < n; i++)
-        {
-           for (int j = 0; j < n; j++)
-           {
-               vvtest[i].push_back(0);
-           }
-           
-        }
-
-        int R_size = n, C_size = n;
-        // cout<<R_size<<C_size<<endl;
-        int vv_bool[R_size][C_size] = {0,0} ;
-        for (int i = 0; i < R_size; i++)
-        {
-           for (int j = 0; j < C_size; j++)
-           {
-            //   cout<<vv_bool[i][j]<<" ";
-            vv_bool[i][j] = 0;
-           }
-
-           
-        }
-       
-        
-        int r = 0,c = 0;
-        int dic_r[4] = {0,1,0,-1};
-        int dic_c[4] = {1,0,-1,0};
-        int det = 0;
-        // 前进的方向
-        for (int i = 0; i < R_size * C_size; i++)
-        {
-            // cout<<matrix[r][c]<<" i "<<i<<endl;
-            // ans.push_back(matrix[r][c]);
-            vv_bool[r][c] = i + 1;
-            vvtest[r][c] = i + 1;
-            int cr = r + dic_r[det];
-            int cc = c + dic_c[det];
-            // cout<<"det "<<det<<" cr "<<cr<<" cc "<<cc<<endl;
-            
-            
-            // 确保 matrix[r][c] 没有超出范围
-            if (0 <= cc && cc < C_size && 0 <= cr && cr < R_size  && vv_bool[cr][cc] == 0)
-            {
-            
-                // 还在范围内就继续直走
-                r = cr;
-                c = cc;
-                // cout<<" r " <<r<<" c "<<c<<endl;
-
-            }
-            else
-            {
-                // cout<<"转弯"<<endl;
-                // 不在范围内就转个弯
-                det = (det + 1) % 4;
-                // cout<<" else det "<<det<<endl;
-                r = r + dic_r[det];
-                c = c + dic_c[det];
-                // cout<<" r " <<r<<" c "<<c<<endl;
                 
-
             }
-            
             
         }
         
-        return vvtest;
-        
+        return obstacleGrid[m - 1][n - 1];
+
     }
 
 };
@@ -206,19 +106,19 @@ void printString(string out){
 }
 int main(int argc,char** argv){
     
-    cout<<"31 "<<endl;
+    cout<<"62 "<<endl;
     vector<vector<int>> nums;
     nums.resize(3);
     
-    nums[0].push_back(1);
-    nums[0].push_back(2);
-    nums[0].push_back(3);
-    nums[1].push_back(4);
-    nums[1].push_back(5);
-    nums[1].push_back(6);
-    nums[2].push_back(7);
-    nums[2].push_back(8);
-    nums[2].push_back(9);
+    nums[0].push_back(0);
+    nums[0].push_back(0);
+    nums[0].push_back(0);
+    nums[1].push_back(0);
+    nums[1].push_back(1);
+    nums[1].push_back(0);
+    nums[2].push_back(0);
+    nums[2].push_back(0);
+    nums[2].push_back(0);
     // nums[3].push_back(15);
     // nums[3].push_back(18);
     
@@ -254,15 +154,20 @@ int main(int argc,char** argv){
     
     
     Solution s;
-    int size =nums.size();
+    // int size =nums.size();
+    int result;
+    result = s.uniquePathsWithObstacles(nums);
+    cout<< result;
+    
+
     // for_each(nums.begin(),nums.end(),printVV);
     // for_each(nums.begin(),nums.begin() + size,[](int ele){cout<<ele<<" ";});
     cout<<endl;
-    vector<vector<int>> vvResult ;
+    // vector<vector<int>> vvResult ;
     // vector<int> vResult;
     // vResult =  s.spiralOrder(nums);
-    vvResult = s.generateMatrix(4);
-    for_each(vvResult.begin(),vvResult.end(),printVV);
+    // vvResult = s.generateMatrix(4);
+    // for_each(vvResult.begin(),vvResult.end(),printVV);
     // for_each(vResult.begin(),vResult.end(),[](int ele){cout<<ele<<" ";});
   
     return 0;
