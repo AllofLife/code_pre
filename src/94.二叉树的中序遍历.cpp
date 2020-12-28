780b96479f3c97d5f45f6eff0f3f40da5159d73e
AllofLife <742415987@qq.com>
2020-12-28 23:56:25 +0800

    94 二叉树的中序遍历 循环写法 提交出错

---
+++ src/94.二叉树的中序遍历.cpp
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void traverse(TreeNode* root,vector<int>& result)
+    {
+        if(root == nullptr)
+        {
+            return;
+        }
+        traverse(root->left,result);
+        result.push_back(root->val);
+        traverse(root->right,result);
+    }
+    vector<int> inorderTraversal_re(TreeNode* root) 
+    {
+    
+        vector<int> result;
+        
+        traverse(root,result);
+        return result;
+        
+    }
+    vector<int> inorderTraversal(TreeNode* root) 
+    {
+        if(root == nullptr)
+        {
+            return {};
+        }
+        vector<int> result;
+        stack<TreeNode*> sta;
+        sta.push(root);
+        TreeNode* cur  = root;
+        while (cur != nullptr && sta.size() != 0 )
+        {
+            // 找到当前子树的最左边叶节点的左节点
+            while (cur != nullptr)
+            {
+               cur = cur->left;
+               sta.push(cur);
+            }
+            // 当前子树的最左边的叶子节点 的左节点为空
+            sta.pop();
+            // 取出当前子树的最左边的叶子节点
+            cur = sta.top();
+            // 中序访问
+            result.push_back(cur->val);
+            // 访问完了之后 将叶子节点弹出
+            sta.pop();
+            // 叶子节点的右子树
+            cur = cur->right;
+            // 换了一棵树 切换到了叶子节点的右子树
+            sta.push(cur);
+            
+        }
+    }
+};
+// @lc code=end
+
