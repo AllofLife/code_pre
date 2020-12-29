b86734c219d56766321a15646c38fad9834c1c18
AllofLife <742415987@qq.com>
2020-12-29 09:36:26 +0800

    144 完结 递归写法和中序一摸一样 循环写法 注意节点进栈和出栈的顺序是时间节点。 首先 栈是空的，先把根节点放进去 访问根节点 循环放入左孩子节点 出栈的时候 先取出栈顶元素，此时栈顶是一个根节点（任何放进去的节点都是“根节点”。） “根节点”出栈 访问 “根节点”的右孩子节点。

--- src/144.二叉树的前序遍历.cpp
+++ src/144.二叉树的前序遍历.cpp
@@ -16,10 +16,68 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#include<vector>
+#include<stack>
+using namespace std;
+
+// struct TreeNode {
+//      int val;
+//      TreeNode *left;
+//      TreeNode *right;
+//      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  };
 class Solution {
 public:
-    vector<int> preorderTraversal(TreeNode* root) {
+    void traverse_first_order(TreeNode* root,vector<int>& ans)
+    {
+        if(root == nullptr)
+        {
+            return ;
+        }
+        ans.push_back(root->val);
+        traverse_first_order(root->left,ans);
+        traverse_first_order(root->right,ans);
+
+    }
+    vector<int> preorderTraversal(TreeNode* root)
+    {
+        if(root == nullptr)
+        {
+            return {};
+        }
+        stack<TreeNode*> s;
+        vector<int> ans;
+        TreeNode* cur = root;
+        while (cur != nullptr || !s.empty())
+        {
+            while (cur != nullptr)
+            {
+                s.push(cur);
+                ans.push_back(cur->val);
+                cur = cur->left;
+            }
+            // // 将空的 节点 出栈
+            // s.pop();  
+            // 取出 栈顶元素
+            cur = s.top() ;
+            // 将根节点出栈
+            s.pop();
+            // 访问右子树
+            cur = cur->right;
+        }
+        return ans;
         
+    }
+    vector<int> preorderTraversal_traverse(TreeNode* root) {
+        if(root == nullptr)
+        {
+            return {};
+        }
+        vector<int> ans;
+        traverse_first_order(root,ans);
+        return ans;
     }
 };
 // @lc code=end
